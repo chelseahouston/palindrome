3b13ae452177bc1cf0454b296e4390bd078deffa
Chelsea Houston <chelseahouston29@gmail.com>
2020-10-25 18:27:15 +0000

    Add option to run again

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-# PROGRAM TO CHECK IF WORD IS PALINDROME
+# PROGRAM TO CHECK IF WORD IS PALINDROME ---
 
-# WORD INPUT
-word = input(str("Enter a word or number to see if it's a Palindrome: "))
+def palindrome():
+# WORD INPUT ---
+  word = input(str("Enter a word or number to see if it's a Palindrome: "))
 
-# IS WORD SAME BACKWARDS AS FORWARDS:
-# Make copy in reverse order
-if word == word[::-1]:
-  print(f"{word} is a Palindrome!")
-else:
-  print(f"{word} is not a Palindrome.")
+# IS WORD SAME BACKWARDS AS FORWARDS ---
+# Make copy in reverse order ---
+  if word == word[::-1]:
+    print(f"{word} is a Palindrome!")
+  else:
+    print(f"{word} is not a Palindrome.")
+  
+# RUN AGAIN? ---
+def run():
+  while True:
+    runagain = input("Run again? (Y/N): ").upper()
+    if runagain not in ('Y', 'N'):
+      print("Invalid Input.")
+      run()
+      break
+    if runagain == 'Y':
+      palindrome()
+      run()
+      break
+    else:
+      print("Have a good day.")
+      break
+
+palindrome()
+run()
