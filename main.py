b5555bd1d205cb60eaf808df397ec37f5eb2b750
Chelsea Houston <chelseahouston29@gmail.com>
2020-10-25 15:49:05 +0000

    Edit comment explaining program

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 word = input(str("Enter a word to see if it's a Palindrome: "))
 
 # IS WORD SAME BACKWARDS AS FORWARDS:
-# Using double colon :: to make copy in reverse order
+# Make copy in reverse order
 if word == word[::-1]:
   print(f"{word} is a Palindrome!")
 else:
